f294309dc0be9be4dc116448f6cea5235e7351d6
Danish Raja <danishimran889@gmail.com>
2022-03-06 12:12:48 +0500

    rebuild

--- src/ApnaBike/BaseUrl.js
+++ src/ApnaBike/BaseUrl.js
@@ -1,2 +1,2 @@
-export const BaseUrl = "http://localhost:3001/";
-// export const BaseUrl = "http://bae2-119-160-69-129.ngrok.io/";
+// export const BaseUrl = "http://localhost:3001/";
+export const BaseUrl = "http://localhost/apnabike-backend/";
